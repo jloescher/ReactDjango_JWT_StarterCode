e87510579dfc5bcde1be293f919e8db2b75bafd3
Jonathan Loescher <jonathan@jloescher.com>
2023-04-15 15:40:41 -0400

    Added Comments App Created Models

---
+++ backend/comments/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=15)
+    text = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "replies"
